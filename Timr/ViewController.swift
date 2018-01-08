0b820845ea54da7719da756d912cf5d29a0a8f73
Jan Dammshäuser <jan.dammshaeuser@7p-group.com>
2018-01-08 20:27:22 +0100

    Initial Commit

---
+++ Timr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timr
+//
+//  Created by Dammshäuser, Jan, SEVEN PRINCIPLES on 08.01.18.
+//  Copyright © 2018 Jan Dammshäuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
